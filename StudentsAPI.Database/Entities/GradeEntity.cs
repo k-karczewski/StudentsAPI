054d0c02d0ab4377ea79d981fdd91a18b886c0b5
Konrad Karczewski <kkarczewski94@gmail.com>
2021-01-06 18:26:28 +0100

    Created Grade entity

---
+++ StudentsAPI.Database/Entities/GradeEntity.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StudentsAPI.Database.Entities
+{
+    public class GradeEntity
+    {
+        public int Id { get; set; }
+        public string GradeValue { get; set; }
+        public string Subject { get; set; }
+
+        public int StudentId { get; set; }
+        public virtual StudentEntity Student { get; set; }
+    }
+}
