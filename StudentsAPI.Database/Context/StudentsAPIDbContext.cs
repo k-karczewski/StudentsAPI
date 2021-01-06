d7ab28096f75c35b941189fe2b0d89fa80146f10
Konrad Karczewski <kkarczewski94@gmail.com>
2021-01-06 17:39:36 +0100

    First version of DbContext

---
+++ StudentsAPI.Database/Context/StudentsAPIDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using StudentsAPI.Database.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StudentsAPI.Database.Context
+{
+    public class StudentsAPIDbContext : DbContext
+    {
+        public DbSet<StudentEntity> Students { get; set; }
+
+        public StudentsAPIDbContext(DbContextOptions options) : base(options) { }
+    }
+}
