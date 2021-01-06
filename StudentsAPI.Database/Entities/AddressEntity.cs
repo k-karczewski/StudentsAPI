f4c31298d9e587631b9815806f30a7543a6c66d2
Konrad Karczewski <kkarczewski94@gmail.com>
2021-01-06 18:18:45 +0100

    Created Address entity

---
+++ StudentsAPI.Database/Entities/AddressEntity.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StudentsAPI.Database.Entities
+{
+    public class AddressEntity
+    {
+        public int Id { get; set; }
+        public string Street { get; set; }
+        public string City { get; set; }
+        public string Country { get; set; }
+
+        public int StudentId { get; set; }
+        public virtual StudentEntity Student { get; set; }
+    }
+}
